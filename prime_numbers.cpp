b620f57bd5480e096574d199c06673d425169226
Suraj Bhardwaj <114643115+surajb1515@users.noreply.github.com>
2022-10-27 15:42:10 +0530

    Create prime_numbers.cpp

---
+++ prime_numbers.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1e7+10;
+const int mod = 1e9+7;
+
+
+
+
+
+
+bool isPrime(int n){
+    if(n == 1) return false;
+    for(int i = 2; i<=n-1; i++){
+        if(n%2 == 0) return false;
+    }
+    return true;
+}
+
+
+
+
+
+signed main()
+{
+   
+    
+    
+    return 0;
+}
